405e251827c6267478d007aba403a02dc61a8cf4
sattawut223@gmail.com <suckseed123>
2023-02-09 18:52:27 +0700

    first commit

--- app.js
+++ app.js
@@ -1,44 +1,26 @@
 const express = require('express');
 const POST = process.env.PORT || 3001;
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+   /* --- API --- */
+const barRouter = require('./router/bar');
 
-// var logger = require('morgan');
-   // * Router >>>> user <<<<
-let register = require('./router/register');
-let login = require('./router/login');
-let login_email = require('./router/login_email');
-let resetpass = require('./router/resetpass');
-let otp = require('./router/otp');
-let user = require('./router/user');
-let profile = require('./router/profile');
-let checkIn = require('./router/checkin');
-let checkOut = require('./router/checkout');
-   // * Router >>>> Admin <<<<
-let admin = require('./router/admin');
-   // * Router >>>> Supper Admin <<<<
-let SupperAdmin = require('./router/supper_admin');
-   // * app
-let app = express();
-   // * use/set
+   /* --- App --- */
+const app = express();
+   /* --- Cors --- */
+const corsOptions ={
+   origin:'http://localhost:3000', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+   /* --- use/set --- */
 app.set('view engine', 'ejs'); // ejs
 app.use(express.json());
 app.use(cookieParser());
-
-   // * router >>>> User <<<<
-app.use('/register',register);
-app.use('/login',login);
-app.use('/loginemail',login_email); 
-app.use('/resetpassword',resetpass);
-app.use('/otp',otp);
-app.use('/user',user);
-app.use('/profile', profile);
-app.use('/checkIn',checkIn);
-app.use('/checkOut',checkOut);
-   // * router >>>> Admin <<<<
-app.use('/admin',admin);
-   // * router >>>> Supper Admin <<<<
-app.use('./supperadmin',SupperAdmin);
-// ////////////////////////////////////////
+   /* --- router --- */
+app.use('/api',barRouter);
+   /* --- Post --- */
 app.listen(POST, () => {
     console.log(`Server running on port ${POST}`)
 })
